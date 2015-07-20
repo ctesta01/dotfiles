1ff09091ce123541022cbbb0b6163eb3e254870e
Christian Testa <ctesta000@gmail.com>
2015-07-20 13:09:27 -0400

    Added setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This is written to setup these dotfiles for a
+# user on a centos box, and haven't been tested anywhere else.
+
+ln -fs ~/dotfiles/.bashrc ~/.bashrc
+ln -fs ~/dotfiles/.vimrc ~/.vimrc
+ln -fs ~/dotfiles/.gitconfig ~/.gitconfig
+ln -fs ~/dotfiles/.tmux.conf ~/.tmux.conf
