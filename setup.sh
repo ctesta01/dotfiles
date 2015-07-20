1cdc3ccd31b674a79d1d8c2776028e544e889b3c
Christian Testa <ctesta000@gmail.com>
2015-07-20 13:12:51 -0400

    Added warning to setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,9 @@
 # This is written to setup these dotfiles for a
 # user on a centos box, and haven't been tested anywhere else.
 
+echo "WARNING: This will overwrite your dotfiles (.bashrc, .vimrc, .gitconfig, .tmux.conf)" 
+read -rsp $'Press any key to continue...\n' -n1 key
+
 ln -fs ~/dotfiles/.bashrc ~/.bashrc
 ln -fs ~/dotfiles/.vimrc ~/.vimrc
 ln -fs ~/dotfiles/.gitconfig ~/.gitconfig
